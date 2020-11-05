ce31b4ce6e828326ea22a469fb834d00f2b76189
AnhelinaDiakova <Gelja4ka@gmail.com>
2020-11-06 00:11:45 +0200

    add src path

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import images from "./js/gallery-items.js";
-import './css/main.css';
+import images from "src/js/gallery-items.js";
+import 'src/css/main.css';
 // console.log(images);
 // <li class="gallery__item">
 //   <a
